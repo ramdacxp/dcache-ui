27d6b6f1bfbf832538558273e4c3963ecea0a2ae
Michael Hoser <michael.hoser@outlook.com>
2024-10-06 20:53:37 +0200

    Added AlpineJS for token handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(
-    {
+  integrations: [
+    tailwind({
       // disable default base.css
       applyBaseStyles: false,
 
       // allow nested CSS
-      nesting: true
-    }
-  )]
+      nesting: true,
+    }),
+    alpinejs({
+      // custom alpinejs entrypoint to add alpinejs plugins
+      entrypoint: '/src/alpinejs-entrypoint'
+    }),
+  ],
 });
